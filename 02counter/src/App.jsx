d4aa95993c11a4bbf2a0c122a115c69037110d84
abhishekkhati-qmf <abhishekkhati6235@gmail.com>
2025-03-13 22:45:33 +0530

    basic counter projefct

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    let [counter,setCounter] = useState(15);
+
+   const addvalue = ()=>{
+    console.log("value added",counter);
+    counter = counter+1;
+    if(counter > 20){
+      counter = 20;
+    }
+    setCounter(counter);
+   }
+
+   const removevalue = ()=>{
+    counter = counter -1;
+    if(counter < 0){
+      counter = 0;
+    }
+    setCounter(counter);
+   }
+
+  return (
+    <>
+       <h1>Mahatma Memer</h1>
+       <h2>Counter Value: {counter}</h2>
+       <button onClick={addvalue}>Add Value  {counter}</button>
+       <br/>
+       <button onClick={removevalue}>Remove Value  {counter}</button>
+    </>
+  )
+}
+
+export default App
